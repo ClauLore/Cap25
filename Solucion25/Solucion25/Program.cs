42420455417e456c93a16b8a52a4ed74082a0270
sr v studio1 <S100VSSabPM@hotmail.com>
2019-11-02 19:51:04 -0500

    vesion master 1.0

---
+++ Solucion25/Solucion25/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solucion25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola mundo");
+
+            Console.ReadLine();
+
+        }
+    }
+}
